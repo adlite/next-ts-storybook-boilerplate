2c49ece43a8b862ec05713ff0eb9103a2818b4a5
Adlite <adlite@yandex.ru>
2019-11-19 16:06:34 +0300

    next.config.js: add CssoWebpackPlugin

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
+// Next plugins
 const withSass = require('@zeit/next-sass');
+// Webpack plugins
+const CssoWebpackPlugin = require('csso-webpack-plugin').default;
 
 const IS_DEV = process.env.NODE_ENV === 'development';
 
 const config = {
-  webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
-    return config
+  webpack(config) {
+    if (!IS_DEV) {
+      config.plugins.push(new CssoWebpackPlugin());
+    }
+    return config;
   },
   cssModules: true,
   cssLoaderOptions: {
