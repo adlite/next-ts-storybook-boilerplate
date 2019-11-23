6b9928e74aadec2570a64ef158dee4574cd1709c
Adlite <adlite@yandex.ru>
2019-11-23 22:57:03 +0300

    ESLint: add eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,28 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: ['react', 'import'],
+  rules: {
+    'import/order': 2,
+    'import/first': 2,
+    'import/exports-last': 2,
+    'import/no-duplicates': 2,
+    'import/default': 2,
+    'import/newline-after-import': 2,
+    'import/no-absolute-path': 2,
+    'import/no-webpack-loader-syntax': 2,
+    'import/no-self-import': 2,
+    'import/no-cycle': 2,
+    'import/no-useless-path-segments': 2,
+    'import/no-relative-parent-imports': 2,
+    'import/no-deprecated': 2,
+    'import/no-mutable-exports': 2,
+    'import/max-dependencies': [2, {max: 15}],
+    'import/no-extraneous-dependencies': [
+      2,
+      {devDependencies: false, optionalDependencies: false, peerDependencies: false},
+    ],
+  },
   settings: {
     // eslint-plugin-react settings
     react: {
