d92637cb5dfe413d3f3ef385ebcaf9775054f8ca
Adlite <adlite@yandex.ru>
2019-11-24 19:33:44 +0300

    Fix decorators support

---
+++ .babelrc.js
@@ -0,0 +1,16 @@
+/**
+ * Babel config extended from Next babel preset
+ * @see https://nextjs.org/docs#customizing-babel-config
+ * @see https://babeljs.io/docs/en/
+ */
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        decoratorsBeforeExport: true,
+      },
+    ],
+  ],
+};
