429df871e8c1ddf8b39779b9e6e8a9ce60047098
Adlite <igor@chillicode.ru>
2021-09-28 06:58:46 +0300

    Change icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,11 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="192x192" href="/icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="512x512" href="/icon-512x512.png" />
           <link rel="manifest" href="/site.webmanifest" />
-          <meta name="theme-color" content="#ffffff" />
+          <meta name="theme-color" content="#E1EBF3" />
         </Head>
         <body>
           <Main />
